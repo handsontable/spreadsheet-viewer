1ba02fc5750d8d8240dc94b4d54c30abb54a1ea9
Marcin Warpechowski <warpech@gmail.com>
2021-10-14 20:25:25 +0200

    make the code closer to the StackBlitz Parcel example

--- src/quickstart/examples/jsapi/js/loading/src/index.js
+++ src/quickstart/examples/jsapi/js/loading/src/index.js
@@ -1,17 +1,17 @@
-import { SpreadsheetViewer } from "tmpsv";
+import { SpreadsheetViewer } from 'tmpsv';
 
 SpreadsheetViewer({
-  container: document.querySelector("div#spreadsheet-viewer"),
-  //assetsUrl: "https://unpkg.com/tmpsv@0.0.1/spreadsheet-viewer/sv/index.html", // ok
+  container: document.querySelector('div#spreadsheet-viewer'),
+  // assetsUrl: "https://unpkg.com/tmpsv@0.0.1/spreadsheet-viewer/sv/index.html", // ok
   assetsUrl:
-    "https://cdn.skypack.dev/tmpsv@0.0.1/spreadsheet-viewer/sv/index.html" // ok
+    'https://cdn.skypack.dev/tmpsv@0.0.1/spreadsheet-viewer/sv/index.html' // ok
 })
   .then((instance) => {
     instance.configure({
-      licenseKey: "evaluation" // contact us to order a license key for commercial use beyond evaluation
+      licenseKey: 'evaluation' // contact us to order a license key for commercial use beyond evaluation
     });
     instance.loadWorkbook(
-      "https://tne.pl/github/spreadsheet-viewer-quickstart/common/workbooks/sample-file.xlsx",
+      'https://tne.pl/github/spreadsheet-viewer-quickstart/common/workbooks/sample-file.xlsx',
       0
     ); // also available: loading from Base64 and ArrayBuffer
   })
