4ffb40c8a511ea1bd266cebd49b075f972ef3e66
Marcin Warpechowski <warpech@gmail.com>
2021-10-26 00:43:43 +0200

    fix the script on Windows

--- docs/.vuepress/docs-links.js
+++ docs/.vuepress/docs-links.js
@@ -2,7 +2,7 @@ const { fs, path, parseFrontmatter } = require('@vuepress/shared-utils');
 const helpers = require('./helpers');
 
 module.exports = function(src) {
-  const version = this.resourcePath.split('/docs/').pop().split('/')[0];
+  const version = this.resourcePath.split(`${path.sep}docs${path.sep}`).pop().split(path.sep)[0];
   const latest = helpers.getLatestVersion();
   const basePath = this.rootContext;
 
